46822d2492e7792c22e65c2ef961ad7901dab65d
Ganesh - Wordpress Developer <dnyanraja2011@gmail.com>
2016-12-14 17:37:35 +0530

    Update shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -1,6 +1,12 @@
  <?php 
-	echo '<div class="css_widget" style="max-width:300px;">';
-	echo  '<h2 class="widgettitle">Sign up to stay in touch!</h2>';
-	constantcontact_form($atts['ca'], $atts['list'], $atts['firstname']);
+ /* @Constant Contact Email Singup - Shortcode */
+	echo '<div class="css_widget" style="max-width:300px;">';	
+	if(!empty($atts['ca']) && !empty($atts['list']) && !empty($atts['firstname'])){ 
+				echo  '<h2 class="widgettitle">Sign up to stay in touch!</h2>';
+				constantcontact_form($atts['ca'], $atts['list'], $atts['firstname']);
+		}
+		else{
+			echo '<p>Please setup CA Input, List Input & First Name attribute for shortcode.</p>';
+		}	
 	echo '</div>';
  ?>
